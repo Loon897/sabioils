f43b505b3acfa0a7eca1c97b82a9fecd914d5cce
Hosea Manhanga <loon897@gmail.com>
2017-02-11 15:39:52 +0200

    Added html templates

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "base2.html", {})
+
+def carousel(request):
+	return render(request, "carousel.html", {})
+
+
+def item(request):
+	return render(request, "product_item.html", {})
--- src/sabioils/settings.py
+++ src/sabioils/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sabioils.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
